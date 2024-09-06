21c4c7bd0b175929bbf0ae4dd46508458f4fe1e8
Rami Alouta <rami8629@esri.com>
2024-09-06 11:33:53 +0200

    added environment check exercise

--- test.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-:: Save the current directory (the directory where the batch file was run from)
-set "ORIGINAL_DIR=%CD%"
-
-:: Print the original directory to verify
-"%ORIGINAL_DIR%"
